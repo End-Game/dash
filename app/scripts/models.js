516c572bcd6dd27e39612ecee48ad93f53cd0f08
Eilis Byrne <eilis@hoistapps.com>
2013-12-06 17:07:38 +1300

    help desk view added

--- app/scripts/models.js
+++ app/scripts/models.js
@@ -1,10 +1,40 @@
 define(['exports', 'dash', 'backbone', "jquery", "hoist", 'relational'], function(exports, Dash, Backbone, $, hoist) {
     'use strict';
+    Backbone.Relational.store.addModelScope(Dash);
+
+    Dash.idCount = 1;
+
+    Dash.checkIdCount = function(id) {
+        if (id >= Dash.idCount) {
+            Dash.idCount = id + 1;
+        }
+    };
+
+    Dash.assignId = function(model) {
+        if (model.get('id') === undefined) {
+            model.set({"id": Dash.idCount});
+            Dash.idCount++;
+        } else {
+            Dash.checkIdCount(model.get("id"));
+        }
+    };
 
     Dash.Product = Backbone.RelationalModel.extend({
         initialize: function() {
-            this.keySections = new Dash.Sections(this.toJSON().keySections);
+            //this.sections = new Dash.Sections(this.toJSON().sections);
+            Dash.assignId(this);
         },
+
+        relations: [{
+            type: Backbone.HasMany,
+            key: 'sections',
+            relatedModel: 'Section',
+            collectionType: 'Sections',
+            autofetch: true,
+            reverseRelation: {
+                key: 'product'
+            }
+        }],
 
         defaults: {
             name: "",
@@ -12,19 +42,63 @@ define(['exports', 'dash', 'backbone', "jquery", "hoist", 'relational'], functio
             description: "",
             helpDeskUrl: "",
             sections: "", // sections collection
-            keySections: "" // sections collection
+        },
+
+        getKeySections: function() {
+            var keySections = new Dash.Sections();
+            this.get('sections').each(function(section) {
+                if (section.get("isKey")) {
+                    keySections.push(section);
+                }
+            });
+            return keySections;
+        },
+
+        getFaqs: function() {
+            var faqs = new Dash.Sections();
+            this.get('sections').each(function(section) {
+                if (section.get("type") === "faq") {
+                    faqs.add(section);
+                }
+            });
+            return faqs;
         }
     });
 
+    // Dash.ProductToSection = Backbone.RelationalModel.extend({
+    //     initialize: function() {
+    //         console.log(this);
+    //     },
+    //     relations: [{
+    //         type: Backbone.HasOne,
+    //         key: 'section',
+    //         relatedModel: 'Dash.Section',
+    //         reverseRelation: {
+    //             key: 'products',
+    //             collectionType: 'Dash.Products'
+    //         }
+    //     }]
+    // });
+
     Dash.Products = Backbone.Collection.extend({
-        rootUrl: "products",
         model: Dash.Product
     });
 
-    Dash.Section = Backbone.Model.extend({
+    Dash.Section = Backbone.RelationalModel.extend({
+        initialize: function() {
+            Dash.assignId(this);
+        },
+
+        subModelTypes: {
+            'article': 'Dash.Section.Article',
+            'faq': 'Dash.Section.Article',
+            'howDoI': 'Dash.Section.Article',
+            'section': 'Dash.Section.Section'
+        },
+
         defaults: {
             name: "",
-            type: ""
+            isKey: false
         }
     });
 
@@ -35,9 +109,10 @@ define(['exports', 'dash', 'backbone', "jquery", "hoist", 'relational'], functio
             tags: "",
             relevantOthers: "", // sections collection
             faqs: "", // sections collection
-            howDos: "" // sections collection
+            howDoIs: "", // sections collection
+            published: false
         }
-        // when looking for other articles use the collection attribute that automatically gets set
+        // when looking for other articles in section use the collection attribute that automatically gets set
     });
 
     Dash.Section.Section = Dash.Section.extend({
@@ -47,14 +122,8 @@ define(['exports', 'dash', 'backbone', "jquery", "hoist", 'relational'], functio
     });
 
     Dash.Sections = Backbone.Collection.extend({
-        model: function(attrs, options) {
-            if (attrs.type === "section") {
-                return new Dash.Section.Section(attrs, options);
-            } else {
-                return new Dash.Section.Article(attrs, options);
-            }
-        }
+        model: Dash.Section
     });
-    
+
     return Dash;
 });
--- app/scripts/views.js
+++ app/scripts/views.js
@@ -16,32 +16,53 @@ define(['dash', 'backbone', 'hoist'], function(Dash, Backbone, hoist) {
     SetupProduct->Admin
     PlaceTree  -> Admin when making article
 */
+    
     Dash.testJson = {
         "products": [{
             "name": "product1",
+            "id": 1,
             "shortDescription": "enter a short short description",
             "description": "long block of text blah blah blah blah blah blah blah blah blah blah blah blah",
             "helpDeskUrl": "enterUrl",
-            "sections": [],
-            "keySections": [{
+            "sections": [{
                 "name": "article1",
-                "type": "article"
-            }, {
+                "type": "article",
+                "isKey": true,
+                "id": 3
+            },{
+                "name": "faq",
+                "type": "faq",
+                "isKey": true,
+                "id":4
+            },{
                 "name": "section1",
-                "type": "section"
+                "type": "section",
+                "id":5
             }]
         }, {
             "name": "product2",
+            "id": 2,
             "shortDescription": "enter a short short description",
             "description": "long block of text blah blah blah blah blah blah blah blah blah blah blah blah",
             "helpDeskUrl": "enterUrl",
-            "sections": [],
-            "keySections": [{
+            "sections": [{
                 "name": "article1",
-                "type": "article"
-            }, {
+                "type": "article",
+                "id": 9
+            },{
                 "name": "section1",
-                "type": "section"
+                "type": "section",
+                "isKey": true,
+                "id": 6 
+            },{
+                "name": "faq1",
+                "type": "faq",
+                "isKey": true,
+                "id": 7 
+            },{
+                "name": "faq2",
+                "type": "faq",
+                "id": 8
             }]
         }]
     };
@@ -54,6 +75,16 @@ define(['dash', 'backbone', 'hoist'], function(Dash, Backbone, hoist) {
         render: function() {
             this.$el.html(this.template(this.model.toJSON()));
             return this;
+        },
+
+        events: {
+            "click #product": "helpDesk"
+        },
+        
+        helpDesk: function() {
+            new Dash.View.HelpDesk({
+                model: this.model
+            });
         }
     });
 
@@ -81,20 +112,15 @@ define(['dash', 'backbone', 'hoist'], function(Dash, Backbone, hoist) {
 
         initialize: function() {
             Dash.View.prototype.initialize.apply(this);
-            this.collection = new Dash.Products(Dash.testJson.products); // products
+            this.collection = new Dash.Products(Dash.testJson.products);
             this.render();
         },
 
         render: function() {
-            this.$el.find("homeProduct").remove(); // remove stuff from previous render
-            // render top row of product and short desc
+            this.$el.find("#homeProduct").remove(); // remove stuff from previous render
             var that = this;
             _.each(this.collection.models, function(item) {
-                that.renderProduct(item);
-            }, this);
-            // render bottom row of key sections
-            _.each(this.collection.models, function(item) {
-                that.renderKeySections(item);
+                that.renderProduct(item); // render top row of product and short desc
             }, this);
         },
 
@@ -103,6 +129,7 @@ define(['dash', 'backbone', 'hoist'], function(Dash, Backbone, hoist) {
                 model: item
             });
             this.$("hr").before(homeProductView.render().el);
+            this.renderKeySections(item); // render bottom row of key sections
         },
 
         renderKeySections: function(item) {
@@ -110,13 +137,78 @@ define(['dash', 'backbone', 'hoist'], function(Dash, Backbone, hoist) {
                 model: item
             });
             this.$el.append(keySections.render().el);
+            var that = this;
+            var keySectionsList = item.getKeySections();
+            keySectionsList.each(function(section) {
+                that.renderListItem(section, "#keySections"+item.get('name'));
+            }, this);
+        },
+        
+        renderListItem: function(item, tag){
+            console.log(tag);
+            var listItem = new Dash.ListItem({
+                model: item
+            });
+            this.$(tag).append(listItem.render().el);
         }
     });
 
     Dash.View.HelpDesk = Dash.View.extend({
         el: "#HelpDesk",
+        
+        tagName: "div",
+        className: "keySections",
+        template: _.template($("#keySectionsTemplate").html()),
 
-        initialize: function() {}
+        initialize: function() {
+            Dash.View.prototype.initialize.apply(this);
+            this.render();
+           // this.renderSidebar();
+        },
+        
+        render: function(){
+            this.$el.find("#helpDeskProduct").remove();
+            var helpDeskProduct = new Dash.HelpDeskProduct({
+                model: this.model
+            });   
+            this.$el.prepend(helpDeskProduct.render().el);
+            var faqs = this.model.getFaqs();
+            var that = this;
+            faqs.each(function(item) {
+                that.renderListItem(item, "#faqList");
+            }, this);  
+        },
+        
+        renderListItem: function(item, tag){
+            var listItem = new Dash.ListItem({
+                model: item
+            });
+            this.$(tag).append(listItem.render().el);
+        }
+    });
+    
+    Dash.HelpDeskProduct = Backbone.View.extend({
+        
+        tagName: "div",
+        className: "helpDeskProduct",
+        template: _.template($("#helpDeskTemplate").html()),
+
+        render: function() {
+            this.$el.html(this.template(this.model.toJSON()));
+            return this;
+        }
+        
+    });
+    
+    Dash.ListItem = Backbone.View.extend({
+        
+        tagName: "li",
+        template: _.template($("#listItemTemplate").html()),
+
+        render: function() {
+            this.$el.html(this.template(this.model.toJSON()));
+            return this;
+        }
     });
 
     return Dash;
